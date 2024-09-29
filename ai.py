e1b79a0b265ffd302896f359ae18cee27f6fa84f
Manoj Turaga <manoj.turaga@ku.edu>
2024-09-28 22:11:31 -0500

    Adding more documentation and commenting (WIP)

--- ai.py
+++ ai.py
@@ -1,43 +1,117 @@
-import random
-import battleship
+'''
+Module: ai.py
+Creation Date: September 23rd, 2024
+Author: Connor Forrsital
+Contributors: Connor Forristal, Manoj Turaga
+Sources:
 
+Description:
+    This module is our implemenation for the Battleship AI. This module will act
+    as a player that will provide coordinates in the place of a human. Three levels
+    of AI are supported, where level one gives coordinates at random, level two
+    attacks at random until a coordinate is found, and level three knows all of
+    the opponents placements
+
+Inputs:
+    The placements of your ships
+Outputs:
+    Coordinates of interest
+
+Sources: GeeksforGeeks/Team 42 initial Battleship implementation
+'''
+###############################################################################
+# Imports
+###############################################################################
+import random
+import board_util
+
+###############################################################################
+# Variables
+###############################################################################
+# All AIs use this as a way to know if a particular coordinate was
+# already attacked
 USED_COORDS = set()
 
-# Medium AI
+# The medium AI will maintain a stack of coordinates to visit
+# in the event it found a coordinate that was a hit
 STACK = []
 SHIP_HIT = False 
 
-# Hard AI
+# The hard AI will maintain a list of all the ship placements
+# so you can't hide from its all seeing eye
 OPPONENT_LOCATION = []
     
-    
 def generateAICoords(is_setup=False):
+    """
+    Function: Generate AI Coordinates
+
+    Description: This function can be used to generate randome coodinates.
+                 It can be used as a set up tool or in attacks. For targetted
+                 AI coordinates, the calling entitiy should pop from the
+                 stack correspoinding to the AI level
+
+    Inputs: If this is the setup phase of the game
+    Outputs: Random coordinate and optionally a direction
+    """
+    # We need this in a loop to make sure that we get a unique
+    # random coordinate in all cases
     while True:
-        row = random.choice([i for i in range(battleship.boardSize)])
-        col = random.choice([j for j in range(battleship.boardSize)])
+        # Get a random row and column
+        row = random.choice([i for i in range(board_util.boardSize)])
+        col = random.choice([j for j in range(board_util.boardSize)])
         
+        # If we are in the setup phase, also add a random direction
         if is_setup:
             direction = random.choice(["H", "V"])
             return (row, col, direction)
 
+        # If we are not in the setup phase annd the random coordinate
+        # is unique, return the coordinate back to the calling function
         if (row, col) not in USED_COORDS:
             USED_COORDS.add((row, col))
             return (row, col)
 
-
 def stack_directions(row, col):
+    """
+    Function: Stack Directions 
+
+    Description: This code is used for the level 2 AI, which determines
+                 the next possible coordinates to attack given the current
+                 coordinate was successful hit.
+
+    Inputs: Row and Column
+    Outputs: nothing
+    """
+    # It is possible that the 4 orthogonal coordinates to the current
+    # coordinate either don't exist or have already been used, so make sure
+    # to only use directions that make sense to access
     possible_directions = choose_direction(row, col)
     
+    # Create a list that contains a set of coordinates that the AI
+    # should target next
     for i in range(len(possible_directions)):
         possible_directions[i] = next_location(possible_directions[i], row, col)
         
+    # Add these new coordinates to the stack
     STACK.extend(possible_directions)
-    
-    
 
 def determine_ai_player():
+    """
+    Function: Determine AI Player 
+
+    Description: This code is part of the entire game setup operation where the
+                 the player is able to determine if they want to play against an
+                 AI
+
+    Inputs: User Input
+    Outputs: If the opponent is an AI and it's difficulty
+    """
+    # Initalize the ai player and the difficulty
+    # to indicate that the opponent is a human
     ai_player, difficulty = False, None 
     
+    # Keep accepting inputs until the selections are all
+    # valid
     while True:
         ai_player = input("Do you want to play against an AI? (Y/N): ").strip()
         if ai_player in ["Y", "y", "yes"]:
@@ -79,10 +153,10 @@ def next_location(direction, row, col):
 def choose_direction(row, col):
     possible_directions = ["N", "E", "S", "W"]
     
-    if row + 1 >= battleship.boardSize or (row + 1, col) in USED_COORDS:
+    if row + 1 >= board_util.boardSize or (row + 1, col) in USED_COORDS:
         possible_directions.remove("S")
     
-    if col + 1 >= battleship.boardSize or (row, col + 1) in USED_COORDS:
+    if col + 1 >= board_util.boardSize or (row, col + 1) in USED_COORDS:
         possible_directions.remove("E")
         
     if row - 1 < 0 or (row - 1, col) in USED_COORDS:
--- board_util.py
+++ board_util.py
@@ -1,7 +1,31 @@
-import os
-import platform
+'''
+Module: board_util.py
+Creation Date: September 11th, 2024
+Author: Team 19 Devs
+Contributors: Manoj Turaga
+Sources:
+
+Description:
+    This is the "remnants" of the original battleship program. Due to the nature
+    of the inital program, we place all board related utlity functions in this
+    file. We wanted to reserve logic to the code in main.py
+
+Inputs:
+    None
+Outputs:
+    None
+
+Sources: GeeksforGeeks/Team 42 initial Battleship implementation
+'''
+###############################################################################
+# Imports
+###############################################################################
 import ai
 import events
+
+###############################################################################
+# Variables
+###############################################################################
 
 # Define a variable to store the size of the board
 boardSize = 10
@@ -21,6 +45,9 @@ shipSizes = {
     5: [1, 2, 3, 4, 5]
 }
 
+###############################################################################
+# Procedure 
+###############################################################################
 def printBoard(board):
     """
     Function: Print Board
@@ -41,7 +68,9 @@ def printBoard(board):
         row = [str(cell) for cell in board[i]]
 
         # The reason for all these replaces is to make the game
-        # display the board more cleanly
+        # display the board more cleanly. We know this is goofy
+        # but we thought it would be fun to preserve aS much of
+        # the original logic of the game as possible
         print((f"{i + 1:2} {' '.join(row)}").replace("~", "Q").replace("Q", "~~").replace("O", "Q").replace("Q", "OO").replace("X", "Q").replace("Q", "XX"))
 
 def createEmptyBoard():
@@ -59,32 +88,78 @@ def createEmptyBoard():
     # empty when all the cells in the board are '~'
     return [['~'] * boardSize for _ in range(boardSize)]
 
-def clear_terminal():
-    if platform.system() == "Windows":
-        os.system("cls")
-    else:
-        os.system("clear")
-
 def getCoordinatesInput(): #gets coordinates
-    while True: #loop
-        coordinates = input("Enter the coordinates (e.g. A5): ").strip().upper() #get input and format it
-        if len(coordinates) < 2: #check if too short
-            print("Invalid input. Try again.") #print error message
-            continue #continue asking
-        col, row = coordinates[0], coordinates[1:] #split input into column and row
-        if col in letters and row.isdigit() and 1 <= int(row) <= 10: #check if column and row are valid
-            return letters.index(col), int(row) - 1 #return indices
+    """
+    Function: Get Coordinate Input
+
+    Description: This function is the method that allows the user to input coordinates
+                 of interest, whether that be when placing ships or triggering an
+                 attack
+    
+    Inputs: User Input
+    Outputs: System represenation of inputted coordinate
+    """
+    # We only return from the function when the input is deemed
+    # to be valid
+    while True:
+        # Obtain the row and column from the user in the form
+        # ColRow and remove any format characters while standardizing
+        # the input
+        coordinates = input("Enter the coordinates (e.g. A5): ").strip().upper()
+
+        # The input needs to be at least 2 characters long.
+        # We can then have this condition as an optimization
+        # in trying to determine if the input is valid
+        if len(coordinates) < 2:
+            print("Invalid input. Try again.")
+            continue
+        
+        # We can assume that column names will only be one character
+        # and it will typically be first character, so assume any
+        # trailing characters are the row identifier
+        col, row = coordinates[0], coordinates[1:]
+
+        # If both the row and the column are valid, then we can
+        # convert the strings into their numeric repreesentations
+        # and return back to the user
+        if col in letters and row.isdigit() and 1 <= int(row) <= 10:
+            return letters.index(col), int(row) - 1
+        
+        # If we were not able to parse a valid coordinate from the input,
+        # Indicate that the input is valid and have the loop try again.
         print("Invalid coordinates. Try again.") #if invalid, ask again
 
 def placeShipOnBoard(board, size, shipId, ai_player=False): #places a ship on the board
-        
-    while True: #loop
+    """
+    Function: Place Ships on Board
+
+    Description: This function is responsible for placing ships on the board, for both
+                 AI and not AI players. This is technically not a "utility" function
+                 as the way this is implemented has been changed to accomodate a larger
+                 feature set but we decided to keep it in this module due to its use in
+                 the original implementation
+
+    Inputs: Player Board, Size of the Ship, Ship ID, if the player is an AI
+    Outputs: Modified player board with ship placements
+    """
+    # It could be the case that the AI picks a coordinate that
+    # is invalid. The human does not have to worry about this in
+    # this function because the get coordinates function has the
+    # placement checking
+    while True:
+        # If the current player is an AI, rely on the AI module to
+        # to get the coordinates of the ship placement. Otherwise,
+        # get it from user input
         if ai_player:
-            col, row, orientation = ai.generateAICoords(is_setup=True)
+            row, col, orientation = ai.generateAICoords(is_setup=True)
         else:
             col, row = getCoordinatesInput() #get coordinates
             orientation = input("Enter H (Horizontal) or V (Vertical): ").strip().upper()#get orientation
         
+        # The following three variables are created to help combine common code
+        # The only thing that is different from this point onwards is if we place
+        # the ship horizontally or vertically, so we will define a comparison base
+        # and a set of increment functions to handle the horizontal and vertical case
         base = None
         row_func = None
         col_func = None
@@ -100,32 +175,63 @@ def placeShipOnBoard(board, size, shipId, ai_player=False): #places a ship on th
             col_func = lambda inc : col
 
         else:
+            # The AI is expected to hit this point since the ship generation
+            # is random but if a player reaches here, then they picked an invalid
+            # coordinate
             if not ai_player: print("Invalid orientation. Try again.") #if orientation is invalid
             continue
-
-        if base + size > boardSize or any(board[row_func( i )][col_func( i )] != '~' for i in range(size)): #check fit and availability
+        
+        # If the result of placing the ships means not every coordinate is within
+        # the range of the board or if there are intersecting placements, do not
+        # accept this start coordinate and instead make the player pick again
+        if base + size > boardSize or any(board[row_func( i )][col_func( i )] != '~' for i in range(size)):
             if not ai_player: 
-                print("Invalid placement. Try again.") #invalid placement message
-                continue #continue asking
+                print("Invalid placement. Try again.")
+                continue
 
         else:
+            # Place the ship on the board. At the same time, put these placements
+            # on the AI as well since the 3rd level AI knows exactly where the
+            # ship are
             for i in range(size): #loop through board
                 if not ai_player:  ai.OPPONENT_LOCATION.append((row_func( i ), col_func( i )))
                 board[row_func( i )][col_func( i )] = shipId #place ship
-                
-            print("\nCurrent board:")# show board
-            printBoard(board) #print the board
-            break
             
+            # Show the current player the fruits of their labor
+            if not ai_player:
+                print("\nCurrent board:")
+                printBoard(board)
+            break
 
+def placeShips(board, shipSizes, ai_player=False):
+    """
+    Function: Place Ships
 
-def placeShips(board, shipSizes, ai_player=False): #places multiple ships on the board
-    for i, size in enumerate(shipSizes): #loop through ships
+    Description: This is a function that allows the calling entity to place
+                 n number of ships at once.
+
+    Inputs: Player Board, Sizes of Ships, if the player is an AI
+    Outputs: Modified player board with ship placements
+    """
+    # For every ship size that is to be placed
+    # place that ship on the board and show the fruits of labor
+    # to the user
+    for i, size in enumerate(shipSizes):
         print("\nCurrent board:")
-        printBoard(board) # Print the empty board before placing ships
-        placeShipOnBoard(board, size, f"S{i+1}", ai_player) #place each ship
+        printBoard(board)
+        placeShipOnBoard(board, size, f"S{i+1}", ai_player)
         events.clear_terminal()
 
+def allShipsSunk(shipHits):
+    """
+    Function: All Ships Sunk
 
-def allShipsSunk(shipHits): #checks if all ships are sunk
-    return all(hit == 0 for hit in shipHits.values()) #return True if all ships are sunk
+    Description: This function determines if every ship that was
+                 place is now sunk.
+
+    Inputs: Dictionary mapping ship ids to health
+    Outputs: Boolean indicated if all ships have a health of 0
+    """
+    # all will return true only if all ships in the ship
+    # hits dictionary are 0
+    return all(hit == 0 for hit in shipHits.values())
